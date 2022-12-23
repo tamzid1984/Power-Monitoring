ff8d369969ed9a9ba06680d285a5419d9bd93ef5
Md Tamzidul Islam <79164907+tamzid1984@users.noreply.github.com>
2022-12-23 11:52:03 +0600

    Add files via upload

---
+++ power.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pickle
+import streamlit as st
+from PIL import Image
+import warnings
+warnings.filterwarnings('ignore')
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+def run():
+    # Adding title and Image
+    img1 = Image.open('logo.png')
+    img1 = img1.resize((156,145))
+    st.image(img1,use_column_width=False)
+    st.title("Power Monitor system of Energy Meter")
+
+    # Voltage
+    Voltage = st.number_input('Voltage')
+
+    # Current
+    Current = st.number_input('Current')
+
+    # Power Consumption
+    Power = st.number_input('Power')
+
+    
+
+    if st.button('Submit'):
+        features = [[Voltage, Current, Power]]
+        print(features)
+        prediction = model.predict(features)
+        st.success(
+                f' {(prediction)}'
+            )
+run()
+
+
