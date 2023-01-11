826faeb78d19111503900bd426cd0d81a60bf371
Md Tamzidul Islam <79164907+tamzid1984@users.noreply.github.com>
2023-01-11 15:38:05 +0600

    Update power.py

--- power.py
+++ power.py
@@ -1,13 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split  
-from sklearn.metrics import accuracy_score 
-from sklearn.metrics import confusion_matrix
-from sklearn. metrics import classification_report, roc_auc_score, roc_curve
+
 import pickle
 import streamlit as st
 import matplotlib.pyplot as plt
