95b4dab7cd8051dd5e7fb00080fb6fa164980c1f
Md Tamzidul Islam <79164907+tamzid1984@users.noreply.github.com>
2023-01-11 15:29:08 +0600

    Update power.py

--- power.py
+++ power.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
